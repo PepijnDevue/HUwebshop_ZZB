9bba1fb5db1af20eb8259983fbc35ee7c0a84820
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-03-17 13:00:52 +0100

    Mid-structurechange commit

--- data_transfer.py
+++ data_transfer.py
@@ -5,6 +5,35 @@ import mongo_functions
 import price_functions
 import fit_data_functions
 
+def handle_2c_2(mongo_db):
+    products = mongo_functions.collect_product_data(mongo_db)
+    price_avg = price_functions.get_avg_price(products)
+    print(f"The average price of the products is {price_avg} euros")
+
+def handle_2c_3(mongo_db):
+    products = mongo_functions.collect_product_data(mongo_db)
+    random_product = random.choice(products)
+    max_deviated_product = price_functions.find_max_price_deviation_product(random_product, products)
+    print(f"The product which price deviates the most from the product {random_product['name']} with the price {round(int(random_product['selling_price'])/100, 2)} is {max_deviated_product['name']} with a price of {round(int(max_deviated_product['selling_price'])/100, 2)} euros")
+    
+
+def handle_products(mongo_db, postgre_cursor, postgre_connection):
+    mongo_cursor = mongo_functions.get_products(mongo_db)
+    done = False
+    batch_size = 3000
+    while not done:
+        item_dicts, done = mongo_functions.batch_handler_products(batch_size, mongo_cursor)
+        item_dicts = fit_data_functions.fit_product_data(item_dicts)
+        postgre_functions.products_to_postgre(postgre_cursor, item_dicts, postgre_connection)
+
+def handle_profiles(mongo_db, postgre_cursor, postgre_connection):
+    mongo_cursor = mongo_functions.get_profiles(mongo_db)
+    done = False
+    batch_size = 200000
+    while not done:
+        item_dicts, done = mongo_functions.batch_handler_profiles(batch_size, mongo_cursor)
+        postgre_functions.profiles_to_postgre(postgre_cursor, item_dicts, postgre_connection)
+
 def handle_sessions(mongo_db, postgre_cursor, postgre_connection):
     """
     """
@@ -16,7 +45,6 @@ def handle_sessions(mongo_db, postgre_cursor, postgre_connection):
         item_dicts = fit_data_functions.fit_session_data(item_dicts)
         postgre_functions.sessions_to_postgre(postgre_cursor, item_dicts, postgre_connection)
 
-
 if __name__ == "__main__":
     # Connect to mongoDB
     mongo_db = mongo_functions.open_mongodb()
@@ -24,25 +52,34 @@ if __name__ == "__main__":
     # Connect to postgreDB
     prostgre_cursor, postgre_connection = postgre_functions.open_postgre()
 
+    # Calculate average price
+    # handle_2c_2(mongo_db)
+
+    # Find product which price deviates most from random given product
+    # handle_2c_3(mongo_db)
+
+    # Transfer products
+    handle_products(mongo_db, prostgre_cursor, postgre_connection)
+
+    # Transfer profiles
+    # handle_profiles(mongo_db, prostgre_cursor, postgre_connection)
+
+    # Transfer sessions
+    # handle_sessions(mongo_db, prostgre_cursor, postgre_connection)
+
+    # Save data manipulation in postgre and close postgreDB
+    postgre_functions.close_postgre(prostgre_cursor, postgre_connection)
+
+
+
     # Get product data
     # products = mongo_functions.collect_product_data(mongo_db)
 
     # Get profile data
     # profiles = mongo_functions.collect_profile_data(mongo_db)
 
-    handle_sessions(mongo_db, prostgre_cursor, postgre_connection)
     # Get session data
-    # sessions = mongo_functions.collect_session_data(mongo_db)
-
-    # Calculate average price (2c.2)
-    # price_avg = price_functions.get_avg_price(products)
-    # print(f"The average price of the products is {price_avg} euros")
-
-    # Find product which price deviates most from random given product (2c.3)
-    # random_product = random.choice(products)
-    # max_deviated_product = price_functions.find_max_price_deviation_product(random_product, products)
-    # print(f"The product which price deviates the most from the product {random_product['name']} with the price {round(int(random_product['selling_price'])/100, 2)} is {max_deviated_product['name']} with a price of {round(int(max_deviated_product['selling_price'])/100, 2)}")
-    
+    # sessions = mongo_functions.collect_session_data(mongo_db)    
 
     # Transfer product data to postgreDB (2c. 1)
     # products = fit_data_functions.fit_product_data(products)
@@ -54,9 +91,3 @@ if __name__ == "__main__":
     # Transfer session data to postgreDB
     # sessions = fit_data_functions.fit_session_data(sessions)
     # postgre_functions.sessions_to_postgre(prostgre_cursor, sessions, postgre_connection)
-
-    # Save data manipulation in postgre and close postgreDB
-    postgre_functions.close_postgre(prostgre_cursor, postgre_connection)
-
-
-# TODO: Leeg database, zet alle code om in zelfde structuur als sessions, ook producten die niet recommendable zijn toe voegen
--- mongo_functions.py
+++ mongo_functions.py
@@ -29,14 +29,9 @@ def get_products(db):
 
     return cursor
 
-def collect_product_data(db):
+def batch_handler_products(batch_size, cursor):
     """
-    Get all product data from mongoDB and save only the important and useful data and records
-
-    returns: a list of dicts, each dict resembles one product record
     """
-    cursor = get_products(db)
-
     # a list of keys wanted for the data transfer
     keys = ['_id', 
             'brand', 
@@ -59,6 +54,8 @@ def collect_product_data(db):
     # a list to fill with the usefull data
     item_dicts = []
 
+    count = 0
+
     # loop through the data from mongoDB
     for record in cursor:
         # create a dict with only the necessary info per record
@@ -76,11 +73,61 @@ def collect_product_data(db):
             else:
                 if key in record:
                     item_dict[key] = record[key]
-        recommendable = 'recommendable' in record and record['recommendable']
-        # only save the info of the record if it has the necessary name and id
-        if recommendable and 'name' in item_dict and '_id' in item_dict and 'selling_price' in item_dict:
-            item_dicts.append(item_dict)
-    return item_dicts
+        item_dicts.append(item_dict)
+        count += 1
+        if count > batch_size:
+            return item_dicts, False
+    return item_dicts, True
+
+# def collect_product_data(db):
+#     """
+#     Get all product data from mongoDB and save only the important and useful data and records
+
+#     returns: a list of dicts, each dict resembles one product record
+#     """
+#     cursor = get_products(db)
+
+#     # a list of keys wanted for the data transfer
+#     keys = ['_id', 
+#             'brand', 
+#             'category', 
+#             'color', 
+#             'fast_mover', 
+#             'gender', 
+#             'name', 
+#             'price.selling_price', 
+#             'properties.discount', 
+#             'properties.doelgroep', 
+#             'size', 
+#             'properties.leeftijd', 
+#             'properties.serie', 
+#             'properties.type', 
+#             'sub_category', 
+#             'sub_sub_category', 
+#             'sub_sub_sub_category']
+
+#     # a list to fill with the usefull data
+#     item_dicts = []
+
+#     # loop through the data from mongoDB
+#     for record in cursor:
+#         # create a dict with only the necessary info per record
+#         item_dict = {}
+#         # check if the current record includes the wanted info
+#         for key in keys:
+#             # some info is an additional layer deep
+#             if '.' in key:
+#                 sub_keys = key.split('.')
+#                 key0 = sub_keys[0]
+#                 key1 = sub_keys[1]
+#                 # use if statement to prevent error if the info is absent
+#                 if key0 in record and key1 in record[key0]:
+#                     item_dict[key1] = record[key0][key1]
+#             else:
+#                 if key in record:
+#                     item_dict[key] = record[key]
+#         item_dicts.append(item_dict)
+#     return item_dicts
 
 def get_profiles(db):
     """
@@ -97,19 +144,13 @@ def get_profiles(db):
 
     return cursor
 
-def collect_profile_data(db):
+def batch_handler_profiles(batch_size, cursor):
     """
-    Get all profile data from mongoDB and save only the important and useful data and records
-
-    returns: a list of dicts, each dict resembles one profile record
     """
-    cursor = get_profiles(db)
-
-    # a list to fill with the usefull data
+     # a list to fill with the usefull data
     item_dicts = []
 
-
-
+    count = 0
     # loop through the data from mongoDB
     for record in cursor:
         # create a dict with only the necessary info per record
@@ -127,9 +168,43 @@ def collect_profile_data(db):
 
         # add record to the list
         item_dicts.append(item_dict)
+        count+=1
+        if count > batch_size:
+            return item_dicts, False
 
-    # return the list of records
-    return item_dicts
+    return item_dicts, True
+
+# def collect_profile_data(db):
+#     """
+#     Get all profile data from mongoDB and save only the important and useful data and records
+
+#     returns: a list of dicts, each dict resembles one profile record
+#     """
+#     cursor = get_profiles(db)
+
+#     # a list to fill with the usefull data
+#     item_dicts = []
+
+#     # loop through the data from mongoDB
+#     for record in cursor:
+#         # create a dict with only the necessary info per record
+#         item_dict = {}
+
+#         # save _id
+#         item_dict['_id'] = str(record['_id'])
+#         # save previously recommended if exists and not empty
+#         if 'previously_recommended' in record and len(record['previously_recommended']) > 0:
+#             item_dict['previously_recommended'] = record['previously_recommended']
+        
+#         # add buids if exist and not empty
+#         if 'buids' in record and record['buids'] != None:
+#             item_dict['buids'] = record['buids']
+
+#         # add record to the list
+#         item_dicts.append(item_dict)
+
+#     # return the list of records
+#     return item_dicts
 
 def get_sessions(db):
     """
@@ -148,7 +223,6 @@ def get_sessions(db):
 
 
 def batch_handler_sessions(batch_size, cursor):
-    count=0
     # a list to fill with the usefull data
     item_dicts = []
 
@@ -198,57 +272,58 @@ def batch_handler_sessions(batch_size, cursor):
     return item_dicts, True
 
 
-def collect_session_data(db):
-    """
-    Get all session data from mongoDB and save only the important and useful data and records
 
-    returns: a list of dicts, each dict resembles one session record
-    """
-    cursor = get_sessions(db)
+# def collect_session_data(db):
+#     """
+#     Get all session data from mongoDB and save only the important and useful data and records
 
-    # a list to fill with the usefull data
-    item_dicts = []
+#     returns: a list of dicts, each dict resembles one session record
+#     """
+#     cursor = get_sessions(db)
 
-    # a list of keys wanted for the data transfer
-    keys = ['_id',
-            'buid',
-            'preferences.brand',
-            'preferences.category',
-            'preferences.gender',
-            'preferences.sub_category',
-            'preferences.sub_sub_category',
-            'preferences.promos',
-            'preferences.product_type',
-            'preferences.product_size']
+#     # a list to fill with the usefull data
+#     item_dicts = []
 
-    # loop through the data from mongoDB
-    count = 0
-    for record in cursor:
-        count += 1
-        # only save session from humans
-        if 'user_agent' in record and record['user_agent']['flags']['is_bot'] == False:
-            # create a dict with only the necessary info per record
-            item_dict = {}
-            # check if the current record includes the wanted info
-            for key in keys:
-                # some info is an additional layer deep
-                if '.' in key:
-                    sub_keys = key.split('.')
-                    key0 = sub_keys[0]
-                    key1 = sub_keys[1]
-                    # use if statement to prevent error if the info is absent
-                    if key0 in record and key1 in record[key0]:
-                        item_dict[key1] = record[key0][key1]
-                else:
-                    if key in record:
-                        item_dict[key] = record[key]
+#     # a list of keys wanted for the data transfer
+#     keys = ['_id',
+#             'buid',
+#             'preferences.brand',
+#             'preferences.category',
+#             'preferences.gender',
+#             'preferences.sub_category',
+#             'preferences.sub_sub_category',
+#             'preferences.promos',
+#             'preferences.product_type',
+#             'preferences.product_size']
 
-            # add ordered products
-            if 'order' in record and record['order'] != None:
-                item_dict['products'] = record['order']['products']
+#     # loop through the data from mongoDB
+#     count = 0
+#     for record in cursor:
+#         count += 1
+#         # only save session from humans
+#         if 'user_agent' in record and record['user_agent']['flags']['is_bot'] == False:
+#             # create a dict with only the necessary info per record
+#             item_dict = {}
+#             # check if the current record includes the wanted info
+#             for key in keys:
+#                 # some info is an additional layer deep
+#                 if '.' in key:
+#                     sub_keys = key.split('.')
+#                     key0 = sub_keys[0]
+#                     key1 = sub_keys[1]
+#                     # use if statement to prevent error if the info is absent
+#                     if key0 in record and key1 in record[key0]:
+#                         item_dict[key1] = record[key0][key1]
+#                 else:
+#                     if key in record:
+#                         item_dict[key] = record[key]
 
-            item_dicts.append(item_dict)
-        if count%300000==0:
-            print(count)
+#             # add ordered products
+#             if 'order' in record and record['order'] != None:
+#                 item_dict['products'] = record['order']['products']
 
-    return item_dicts
+#             item_dicts.append(item_dict)
+#         if count%300000==0:
+#             print(count)
+
+#     return item_dicts
