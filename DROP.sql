4097606ef3c0cf16b678d393aa63636621888459
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-03-21 15:39:52 +0100

    Faster dropscript

--- DROP.sql
+++ DROP.sql
@@ -1,8 +1,5 @@
 -- SQL to clear the data in the relational database
 
-DELETE FROM buid;
-DELETE FROM session_order;
-DELETE FROM prev_recommended;
-DELETE FROM user_session;
-DELETE FROM user_profile;
-DELETE FROM product;
+TRUNCATE table user_session cascade;
+TRUNCATE table user_profile cascade;
+TRUNCATE table product cascade;
