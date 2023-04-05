0c756dbaabc3ea6228887fff14ca657b586e84bb
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-04-05 16:31:58 +0200

    v0.1

--- add_to_pg.py
+++ add_to_pg.py
@@ -7,15 +7,16 @@ if __name__ == '__main__':
     # Connect to postgreDB
     cursor, connection = postgre_functions.open_postgre()
 
-    # create_rec_tables.create_table_brand_products(cursor)
-    # create_rec_tables.create_table_group_products(cursor)
-    # create_rec_tables.create_table_series_products(cursor)
-    # create_rec_tables.create_table_sscat_products(cursor)
-    # create_rec_tables.create_table_category_products(cursor)
+    create_rec_tables.create_table_brand_products(cursor)
+    create_rec_tables.create_table_group_products(cursor)
+    create_rec_tables.create_table_series_products(cursor)
+    create_rec_tables.create_table_sscat_products(cursor)
+    create_rec_tables.create_table_category_products(cursor)
+    print('Done with content filter Pepijn')
     create_rec_tables.content_filtering(cursor)
-    print('Done with content filter')
+    print('Done with content filter Dave')
     create_rec_tables.collaborative_filtering(cursor)
-    print('Done with collab filter')
+    print('Done with collab filter Dave')
 
     # save changes and close postgre connection safely
     postgre_functions.close_postgre(cursor, connection)
